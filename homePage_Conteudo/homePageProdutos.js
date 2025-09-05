82b8f1ca097c13d56b8c1acc48978611ec716977
MateusGaioffatto <gaioffattomateus@gmail.com>
2025-09-05 17:10:19 -0300

    Lembre-se: Baby Steps 👍

--- homePage_Conteudo/homePageProdutos.js
+++ homePage_Conteudo/homePageProdutos.js
@@ -17,10 +17,17 @@ function modificarAnunciosDeProdutos(valor) {
 }
 // COMENTAR =>
 function mostrarImagensDosProdutos(searchInputText) {
+  const homePageProdutosUlLi = document.querySelectorAll(".homePageProdutosUl li");
   const url = `http://localhost:3000/api/shopping?q=${encodeURIComponent(searchInputText)}`;
   console.log(url);
   fetch(url)
-  .then(data => console.log(data.json()))
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < homePageProdutosUlLi.length; i++) {
+          homePageProdutosUlLi[i].style.backgroundImage = `url('${data.shopping_results[i].thumbnail}')`;
+        }
+    })
+    .catch(error => console.error('Erro ao buscar produtos:', error));
 }
 
 // Exporta as funções se precisar em outros módulos
